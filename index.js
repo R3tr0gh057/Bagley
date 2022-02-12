d19ed1e3d33ebdfe5e88496536a2761f109153f6
Joe Sanjo <57857214+R3tr0gh057@users.noreply.github.com>
2022-02-12 09:27:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,6 +37,38 @@ client.on('messageCreate', (ctx) => {
         const command = args.shift().toLocaleLowerCase();
         ctx.channel.bulkDelete(`${args}`);
     }
+    else if(ctx.content.startsWith(`${prefix}help`)){
+        const basic = new client.MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle(`Commands of celeste bot : `)
+        .setDescription(`Command executed by ${ctx.author.tag}`)
+        .setTitle(`Basic commands : `)
+        .addFields(
+            { name: 'Check if the bot is active : ', value: `~status`},
+            { name: 'Server info : ', value: `~info`},
+            { name: 'User info : ', value: `~userinfo`},
+            { name: 'Check bot version', value: `~info version`},
+            { name: 'Display the avatar', value: `~avatar`},
+            { name: 'Mention everyone', value: `~listen`}
+        )
+        ctx.channel.send(basic);
+
+        const action = new client.MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle(`Action commands : `)
+        .addFields(
+            {name: 'Punch someone', value: `~punch (@user)`},
+            {name: 'Kill someone', value: `~kill (@user)`},
+            {name: 'Kiss someone', value: `~kiss (@user)`},
+            {name: 'Bite someone', value: `~bite (@user)`},
+            {name: 'Pat someone', value: `~pat (@user)`},
+            {name: 'Love someone', value: `~love (@user)`},
+            {name: 'Destroy someone', value: `~destroy (@user)`},
+            {name: 'Laugh out loud', value: `~lol`},
+            {name: 'Cry', value: `~cry`}
+        )
+        ctx.channel.send(action);
+    }
 })
 
 client.login(process.env.TOKEN)
